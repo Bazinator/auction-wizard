8ecea9354a2e22e1b290748db440fd7e4a3731fc
bazinator <mpeterb@telus.net>
2026-01-16 10:29:28 -0800

    Adding apiSerive.js Improvements

--- auction-wizard-backend/services/itemService.js
+++ auction-wizard-backend/services/itemService.js
@@ -4,7 +4,6 @@ const axios = require('axios');
 const { MongoClient} = require('mongodb');
 const fs = require('fs');
 const dotenv = require('dotenv');
-const { exec }  = require('child_process');
 const path = require('path');
 
 // Load the .env file
@@ -23,7 +22,7 @@ const uri = process.env.MONGODB_URI;
 
 
 
-const { red, green, yellow } = require('colorette');
+const { red, yellow } = require('colorette');
 
 // API Key
 
